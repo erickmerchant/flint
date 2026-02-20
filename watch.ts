3f5c9a03cee933a69fab5f0a25176d7df150033f
erickmerchant <erickmerchant@gmail.com>
2026-02-20 14:26:40 -0500

    Honor gitignore

--- watch.ts
+++ watch.ts
@@ -1,7 +1,11 @@
 import * as Path from "@std/path";
 import { debounce } from "@std/async/debounce";
+import * as GitIgnore from "@cfa/gitignore-parser";
 
-export default function (dist: string): Response {
+export default async function (dist: string): Promise<Response> {
+  const gitignore = GitIgnore.compile(
+    await (Deno.readTextFile(".gitignore").catch(() => "")),
+  );
   let watcher: Deno.FsWatcher;
   const result = new ReadableStream({
     async start(controller) {
@@ -15,7 +19,7 @@ export default function (dist: string): Response {
 
       for await (const e of watcher) {
         const paths = e.paths.filter((p) =>
-          !p.startsWith(Path.join(Deno.cwd(), dist))
+          !p.startsWith(Path.join(Deno.cwd(), dist)) && gitignore.accepts(p)
         );
 
         if (!paths.length) continue;
